e04a268002c02dc3bf9dca3880e9c437d2b7a926
WangYujie <coolwyj@126.com>
2016-11-05 21:32:32 +0800

    Update matcaffe.h

--- windows/matcaffe/matcaffe.h
+++ windows/matcaffe/matcaffe.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <memory>
+#include <thread>
+#include <vector>
 #include "mex.h"
 #include "caffe/caffe.hpp"
 
@@ -7,9 +9,26 @@
 class MultiGPUSolver
 {
 public:
-	MultiGPUSolver();
+	explicit MultiGPUSolver(std::vector <int> gpus);
 	~MultiGPUSolver();
+
+	int  GetMaxIter();
+	int  GetIter();
+	void Restore();
+	void Solve();
+	void Step();
+	void Snapshot();
+	void Reset();
+	caffe::Net<float> GetNet();
+	caffe::Net<float> GetTestNet();
 private:
+	// task
+
+private:
+	// variable
 	std::shared_ptr<caffe::P2PSync<float>> sync_solvers_;
 	std::shared_ptr<caffe::Solver<float>>  root_solvers_;
+	std::vector <int> gpus_;
+	std::vector <std::thread> gpu_controller_thread_;
+	void** gpu_task_ptr_;
 };
